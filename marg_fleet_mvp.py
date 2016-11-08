e6d678b22bf0a13d7a6fbc9bd783e8cf57371828
stella47 <stella47@users.noreply.github.com>
2016-11-08 13:46:08 -0800

    Update marg_fleet_mvp.py

--- marg_fleet_mvp.py
+++ marg_fleet_mvp.py
@@ -14,7 +14,10 @@ from gurobipy import *
 
 
 
-
++#data for charging station, Tim or Varsh to fill out
+ +max_charge_rate = 0 
+ +min_charge_rate = 0
+  
 
 
 
@@ -36,7 +39,7 @@ csvFile = csv.reader(file)
 # Pull out headers
 Header = csvFile.next()
 
-# Make a multidict with all patient information
+# Make a multidict with all route information
 route_name, route_distance, route_hours, service_hours, loops_day, active_evs = multidict({row[0]: row[1:len(Header)] for row in csvFile})
 
 # Fix data type for Age, Waiting Time, and Life Expectancy, since they are all currently stored as strings (characters)
@@ -70,6 +73,32 @@ for h in hour_price:
 print summer_weekend
 
 
-
+# build objective function
+ +
+ +
+ +
+ +
+ +# build decision variables
+ +### route_assignment: for each bus, for each hour, choose route assignment (0,1,2,3,4,5)
+ +
+ +### charge_or_not: for each bus, for each hour, binary decision variable to determine charging (1), or not charging (0)
+ +
+ +### charge_rate: for each bus, for each hour, continuous decision variable of rate of charging, bounded by highest charge rate
+ +
+ +
+ +
+ +# build constraints
+ +## constraint 1: # of buses allowed on the same route at the same time
+ +
+ +## constraint 2: # of hours it takes to complete each route
+ +
+ +## State-of-charge constraint, cannot over charge the batteries to exceed the maximum capacity of the battery
+ +
+ +## Cannot be charging if the bus in on a route
+ +   ##if route_assignment is not 0, charge_or_not = 0 
+ +   ##if route_assignment is 0, charge_or_not can be 0 or 1
+ +   ##if charge_or_not = 0, charge_rate = 0
+ +   ##if charge_or_not = 1, 0<charge_rate<max_charge_rate
+ +
 
 
